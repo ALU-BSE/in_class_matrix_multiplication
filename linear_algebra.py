71503ece2b6afeab58d52aa2272cbf7d6829bc04
m-ogore <42109589+m-ogore@users.noreply.github.com>
2026-01-21 09:21:52 +0200

    Refactor matrix multiplication in linear_algebra.py
    Removed matrix multiplication logic and added a placeholder.

--- linear_algebra.py
+++ linear_algebra.py
@@ -10,11 +10,7 @@ arr_2 = [[1,3,6],
 new_matrix = []
 for row in range(len(arr_1)):
     for col in range(len(arr_2)):
-        sub_result = 0
-        for j in range(len(arr_2[0])):
-            sub_result += arr_1[row][j] * arr_2[j][col]
-        print(sub_result)
-        new_matrix.append(sub_result)
+            pass
         
     #new_el = 0
 print(new_matrix)
